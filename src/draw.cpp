c07189870ca76938cfc12312898d6d212868e851
David Riley <driley@moonfall.com>
2014-08-22 06:49:37 -0700

    lush: add uppercase characters

--- src/draw.cpp
+++ src/draw.cpp
@@ -73,6 +73,238 @@ void draw_char(int x, int y, char c, Colour fg, const Colour *bg)
     case '9':
       bits = 0x69716000;
       break;
+//  XX  6
+// X  X 9
+// XXXX f
+// X  X 9
+// X  X 9
+    case 'A':
+      bits = 0x69f99000;
+      break;
+// XXX  e
+// X  X 9
+// XXX  e
+// X  X 9
+// XXX  e
+    case 'B':
+      bits = 0xe9e9e000;
+      break;
+//
+//  XX  6
+// X  X 9
+// X    8
+// X  X 9
+//  XX  6
+    case 'C':
+      bits = 0x69896000;
+      break;
+//
+// XXX  e
+// X  X 9
+// X  X 9
+// X  X 9
+// XXX  e
+    case 'D':
+      bits = 0xe999e000;
+      break;
+//
+// XXXX f
+// X    8
+// XXX  e
+// X    8
+// XXXX f
+    case 'E':
+      bits = 0xf8e8f000;
+      break;
+// 
+// XXXX f
+// X    8
+// XXX  e
+// X    8
+// X    8
+    case 'F':
+      bits = 0xf8e88000;
+      break;
+//
+//  XX  6
+// X    8
+// X XX b
+// X  X 9
+//  XX  6
+    case 'G':
+      bits = 0x68b96000;
+      break;
+//
+// X  X 9
+// X  X 9
+// XXXX f
+// X  X 9
+// X  X 9
+    case 'H':
+      bits = 0x99f99000;
+      break;
+//
+// XXXX f
+//  XX  6
+//  XX  6
+//  XX  6
+// XXXX f
+    case 'I':
+      bits = 0xf666f000;
+      break;
+//
+//    X 1
+//    X 1
+//    X 1
+// X  X 9
+//  XX  6
+    case 'J':
+      bits = 0x11196000;
+      break;
+//
+// X  X 9
+// X X  a
+// XX   c
+// X X  a
+// X  X 9
+    case 'K':
+      bits = 0x9aca9000;
+      break;
+//
+// X    8
+// X    8
+// X    8
+// X    8
+// XXXX f
+    case 'L':
+      bits = 0x8888f000;
+      break;
+//
+// X  X 9
+// XXXX f
+// XXXX f
+// X  X 9
+// X  X 9
+    case 'M':
+      bits = 0x9ff99000;
+      break;
+//
+// X  X 9
+// XX X d
+// XXXX f
+// X XX b
+// X  X 9
+    case 'N':
+      bits = 0x9dfb9000;
+      break;
+//
+//  XX  6
+// X  X 9
+// X  X 9
+// X  X 9
+//  XX  6
+    case 'O':
+      bits = 0x69996000;
+      break;
+// 
+// XXX  e
+// X  X 9
+// XXX  e
+// X    8
+// X    8
+    case 'P':
+      bits = 0xe9e88000;
+      break;
+//
+//  XX  6
+// X  X 9
+// X  X 9
+// X XX b
+//  XXX 7
+    case 'Q':
+      bits = 0x699b7000;
+      break;
+//
+// XXX  e
+// X  X 9
+// XXX  e
+// X  X 9
+// X  X 9
+    case 'R':
+      bits = 0xe9e99000;
+      break;
+//
+//  XXX 7
+// X    8
+//  XX  6
+//    X 1
+// XXX  e
+    case 'S':
+      bits = 0x7861e000;
+      break;
+//
+// XXXX f
+//  XX  6
+//  XX  6
+//  XX  6
+//  XX  6
+    case 'T':
+      bits = 0xf6666000;
+      break;
+//
+// X  X 9
+// X  X 9
+// X  X 9
+// X  X 9
+// XXXX f
+    case 'U':
+      bits = 0x9999f000;
+      break;
+//
+// X  X 9
+// X  X 9
+// X  X 9
+// X  X 9
+//  XX  6
+    case 'V':
+      bits = 0x99996000;
+      break;
+//
+// X  X 9
+// X  X 9
+// XXXX f
+// XXXX f
+// X  X 9
+    case 'W':
+      bits = 0x99ff9000;
+      break;
+//
+// X  X 9
+// X  X 9
+//  XX  6
+// X  X 9
+// X  X 9
+    case 'X':
+      bits = 0x99699000;
+      break;
+//
+// X  X 9
+// X  X 9
+// X  X 9
+//  XX  6
+//  XX  6
+    case 'Y':
+      bits = 0x99966000;
+      break;
+//
+// XXXX f
+//   XX 3
+//  XX  6
+// XX   c
+// XXXX f
+    case 'Z':
+      bits = 0xf36cf000;
+      break;
     default:
       bits = 0x0;
   }
