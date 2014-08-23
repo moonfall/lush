50f00e0372d035c8bb65c06dc602f683902f8af4
David Riley <driley@moonfall.com>
2014-08-23 01:14:41 -0700

    lush: switch back to 4x5 font

--- src/font.cpp
+++ src/font.cpp
@@ -1,56 +1,286 @@
 #include "lush.h"
 
+#define FONT_4x5
+#ifdef FONT_3x5
 const char FONT_START = ' ';
 const uint32_t FONT_DATA[] = {
-	0x00000000, 0x44404000, // <space> !
-	0xaa000000, 0xeaaea000, // " #
-	0x6ce6c000, 0x2a48a000, // $ %
-	0x4ecae000, 0x44000000, // & '
-	0x24442000, 0x24224000, // ( )
-	0x0a4a0000, 0x40e40000, // * +
-	0x00048000, 0x00e00000, // , -
-	0x00008000, 0x22488000, // . /
-	0x4aaa4000, 0x4c44e000, // 0 1
-	0xc248e000, 0x2e42e000, // 2 3
-	0xaae22000, 0x8ec2c000, // 4 5
-	0x68eae000, 0x2e444000, // 6 7
-	0xea4ae000, 0xaee2c000, // 8 9
-	0x04040000, 0x40048000, // : ;
-	0x24842000, 0xe00e0000, // < =
-	0x84248000, 0x2c404000, // > ?
-	0x4ae86000, 0xa4eaa000, // @ A
-	0xeacae000, 0x8e88e000, // B C
-	0xcaaac000, 0x8ec8e000, // D E
-	0xe8c88000, 0x86aae000, // F G
-	0xaaeaa000, 0x4e44e000, // H I
-	0x222ae000, 0xaacaa000, // J K
-	0x8888e000, 0xeaeaa000, // L M
-	0xcaaaa000, 0xaeaae000, // N O
-	0xeae88000, 0xaeaee000, // P Q
-	0xcacaa000, 0xe8e2e000, // R S
-	0xe4444000, 0xaaaae000, // T U
-	0xaaaa4000, 0xaaeea000, // V W
-	0xaa4aa000, 0xaae44000, // X Y
-	0xe248e000, 0x46446000, // Z [
-	0x88422000, 0x26226000, // \ ]
-	0x4a000000, 0x0000e000, // ^ _
-	0x84000000, 0x00ea6000, // ` a
-	0x08cac000, 0x00686000, // b c
-	0x026a6000, 0x40ac6000, // d e
-	0x02464000, 0x40a6c000, // f g
-	0x08caa000, 0x40044000, // h i
-	0x40448000, 0x80aca000, // j k
-	0x04442000, 0x00eea000, // l m
-	0x00caa000, 0x00eae000, // n o
-	0x04ac8000, 0x40a62000, // p q
-	0x00244000, 0x40848000, // r s
-	0x04642000, 0x00aa6000, // t u
-	0x00aa4000, 0x00aee000, // v w
-	0x00a4a000, 0x00a6c000, // x y
-	0x08484000, 0x46846000, // z {
-	0x04444000, 0x4c24c000, // | }
-	0x02e80000, 0xeeeee000, // ~ <block>
+    0x00000000, 0x44404000, // <space> !
+    0xaa000000, 0xaeaea000, // " #
+    0x6ce6c000, 0xa248a000, // $ %
+    0x4ecae000, 0x44000000, // & '
+    0x24442000, 0x42224000, // ( )
+    0x0a4a0000, 0x04e40000, // * +
+    0x00048000, 0x00e00000, // , -
+    0x00008000, 0x22488000, // . /
+    0x4aaa4000, 0xc444e000, // 0 1
+    0xc248e000, 0xe242e000, // 2 3
+    0xaae22000, 0xe8c2c000, // 4 5
+    0x68eae000, 0xe2444000, // 6 7
+    0xea4ae000, 0xeae2c000, // 8 9
+    0x04040000, 0x04048000, // : ;
+    0x24842000, 0x0e0e0000, // < =
+    0x84248000, 0xc2404000, // > ?
+    0x4ae86000, 0x4aeaa000, // @ A
+    0xeacae000, 0xe888e000, // B C
+    0xcaaac000, 0xe8c8e000, // D E
+    0xe8c88000, 0x68aae000, // F G
+    0xaaeaa000, 0xe444e000, // H I
+    0x222ae000, 0xaacaa000, // J K
+    0x8888e000, 0xaeeaa000, // L M
+    0xcaaaa000, 0xeaaae000, // N O
+    0xeae88000, 0xeaaee000, // P Q
+    0xcacaa000, 0xe8e2e000, // R S
+    0xe4444000, 0xaaaae000, // T U
+    0xaaaa4000, 0xaaeea000, // V W
+    0xaa4aa000, 0xaae44000, // X Y
+    0xe248e000, 0x64446000, // Z [
+    0x88422000, 0x62226000, // \ ]
+    0x4a000000, 0x0000e000, // ^ _
+    0x84000000, 0x00ea6000, // ` a
+    0x08cac000, 0x00686000, // b c
+    0x026a6000, 0x04ac6000, // d e
+    0x02464000, 0x04a6c000, // f g
+    0x08caa000, 0x04044000, // h i
+    0x40448000, 0x08aca000, // j k
+    0x04442000, 0x00eea000, // l m
+    0x00caa000, 0x00eae000, // n o
+    0x04ac8000, 0x04a62000, // p q
+    0x00244000, 0x04848000, // r s
+    0x04642000, 0x00aa6000, // t u
+    0x00aa4000, 0x00aee000, // v w
+    0x00a4a000, 0x00a6c000, // x y
+    0x08484000, 0x64846000, // z {
+    0x04444000, 0xc424c000, // | }
+    0x02e80000, 0xeeeee000, // ~ <block>
 };
+#endif
+#ifdef FONT_4x5
+const char FONT_START = '0';
+const uint32_t FONT_DATA[] = {
+//  XX  6
+// X  X 9
+// X  X 9
+// X  X 9
+//  XX  6
+    0x69996000,
+//  XX  6
+//   X  2
+//   X  2
+//   X  2
+//   X  2
+    0x62222000,
+// XXX  e
+//    X 1
+//   X  2
+//  X   4
+// XXXX f
+    0xe124f000,
+// XXX  e
+//    X 1
+//  XX  6
+//    X 1
+// XXX  e
+    0xe161e000,
+// X  X 9
+// X  X 9
+// XXXX f
+//    X 1
+//    X 1
+    0x99f11000,
+// XXXX f
+// X    8
+// XXX  e
+//    X 1
+// XXX  e
+    0xf8e1e000,
+//  XX  6
+// X    8
+// XXX  e
+// X  X 9
+//  XX  6
+    0x68e96000,
+// XXXX f
+//    X 1
+//   X  2
+//  X   4
+// X    8
+    0xf1248000,
+//  XX  6
+// X  X 9
+//  XX  6
+// X  X 9
+//  XX  6
+    0x69696000,
+//  XX  6
+// X  X 9
+//  XXX 7
+//    X 1
+//  XX  6
+    0x69716000,
+
+    0x04040000, 0x04048000, // : ,
+    0x24842000, 0x0e0e0000, // < =
+    0x84248000, 0xc2404000, // > ?
+    0x4ae86000, // @
+//  XX  6
+// X  X 9
+// XXXX f
+// X  X 9
+// X  X 9
+    0x69f99000,
+// XXX  e
+// X  X 9
+// XXX  e
+// X  X 9
+// XXX  e
+    0xe9e9e000,
+//  XX  6
+// X  X 9
+// X    8
+// X  X 9
+//  XX  6
+    0x69896000,
+// XXX  e
+// X  X 9
+// X  X 9
+// X  X 9
+// XXX  e
+    0xe999e000,
+// XXXX f
+// X    8
+// XXX  e
+// X    8
+// XXXX f
+    0xf8e8f000,
+// XXXX f
+// X    8
+// XXX  e
+// X    8
+// X    8
+    0xf8e88000,
+//  XX  6
+// X    8
+// X XX b
+// X  X 9
+//  XX  6
+    0x68b96000,
+// X  X 9
+// X  X 9
+// XXXX f
+// X  X 9
+// X  X 9
+    0x99f99000,
+// XXXX f
+//  XX  6
+//  XX  6
+//  XX  6
+// XXXX f
+    0xf666f000,
+//    X 1
+//    X 1
+//    X 1
+// X  X 9
+//  XX  6
+    0x11196000,
+// X  X 9
+// X X  a
+// XX   c
+// X X  a
+// X  X 9
+    0x9aca9000,
+// X    8
+// X    8
+// X    8
+// X    8
+// XXXX f
+    0x8888f000,
+// X  X 9
+// XXXX f
+// XXXX f
+// X  X 9
+// X  X 9
+    0x9ff99000,
+// X  X 9
+// XX X d
+// XXXX f
+// X XX b
+// X  X 9
+    0x9dfb9000,
+//  XX  6
+// X  X 9
+// X  X 9
+// X  X 9
+//  XX  6
+    0x69996000,
+// XXX  e
+// X  X 9
+// XXX  e
+// X    8
+// X    8
+    0xe9e88000,
+//  XX  6
+// X  X 9
+// X  X 9
+// X XX b
+//  XXX 7
+    0x699b7000,
+// XXX  e
+// X  X 9
+// XXX  e
+// X  X 9
+// X  X 9
+    0xe9e99000,
+//  XXX 7
+// X    8
+//  XX  6
+//    X 1
+// XXX  e
+    0x7861e000,
+// XXXX f
+//  XX  6
+//  XX  6
+//  XX  6
+//  XX  6
+    0xf6666000,
+// X  X 9
+// X  X 9
+// X  X 9
+// X  X 9
+// XXXX f
+    0x9999f000,
+// X  X 9
+// X  X 9
+// X  X 9
+// X  X 9
+//  XX  6
+    0x99996000,
+// X  X 9
+// X  X 9
+// XXXX f
+// XXXX f
+// X  X 9
+    0x99ff9000,
+// X  X 9
+// X  X 9
+//  XX  6
+// X  X 9
+// X  X 9
+    0x99699000,
+// X  X 9
+// X  X 9
+// X  X 9
+//  XX  6
+//  XX  6
+    0x99966000,
+// XXXX f
+//   XX 3
+//  XX  6
+// XX   c
+// XXXX f
+    0xf36cf000,
+};
+#endif
+
 const int FONT_COUNT = sizeof(FONT_DATA) / sizeof(FONT_DATA[0]);
 const char FONT_END = FONT_START + FONT_COUNT;
 
@@ -129,36 +359,87 @@ void draw_char(int x, int y, char c, Colour fg, const Colour *bg)
   // a single row with a set bit meaning the character has that pixel set.
   int bits;
   switch (c) {
+//  XX  6
+// X  X 9
+// X  X 9
+// X  X 9
+//  XX  6
     case '0':
       bits = 0x69996000;
       break;
+//  XX  6
+//   X  2
+//   X  2
+//   X  2
+//   X  2
     case '1':
       bits = 0x62222000;
       break;
+// XXX  e
+//    X 1
+//   X  2
+//  X   4
+// XXXX f
     case '2':
       bits = 0xe124f000;
       break;
+// XXX  e
+//    X 1
+//  XX  6
+//    X 1
+// XXX  e
     case '3':
       bits = 0xe161e000;
       break;
+// X  X 9
+// X  X 9
+// XXXX f
+//    X 1
+//    X 1
     case '4':
       bits = 0x99f11000;
       break;
+// XXXX f
+// X    8
+// XXX  e
+//    X 1
+// XXX  e
     case '5':
       bits = 0xf8e1e000;
       break;
+//  XX  6
+// X    8
+// XXX  e
+// X  X 9
+//  XX  6
     case '6':
       bits = 0x68e96000;
       break;
+// XXXX f
+//    X 1
+//   X  2
+//  X   4
+// X    8
     case '7':
       bits = 0xf1248000;
       break;
+//  XX  6
+// X  X 9
+//  XX  6
+// X  X 9
+//  XX  6
     case '8':
       bits = 0x69696000;
       break;
+//  XX  6
+// X  X 9
+//  XXX 7
+//    X 1
+//  XX  6
     case '9':
       bits = 0x69716000;
       break;
+
 //  XX  6
 // X  X 9
 // XXXX f
@@ -175,7 +456,6 @@ void draw_char(int x, int y, char c, Colour fg, const Colour *bg)
     case 'B':
       bits = 0xe9e9e000;
       break;
-//
 //  XX  6
 // X  X 9
 // X    8
@@ -184,7 +464,6 @@ void draw_char(int x, int y, char c, Colour fg, const Colour *bg)
     case 'C':
       bits = 0x69896000;
       break;
-//
 // XXX  e
 // X  X 9
 // X  X 9
@@ -193,7 +472,6 @@ void draw_char(int x, int y, char c, Colour fg, const Colour *bg)
     case 'D':
       bits = 0xe999e000;
       break;
-//
 // XXXX f
 // X    8
 // XXX  e
@@ -202,7 +480,6 @@ void draw_char(int x, int y, char c, Colour fg, const Colour *bg)
     case 'E':
       bits = 0xf8e8f000;
       break;
-// 
 // XXXX f
 // X    8
 // XXX  e
@@ -211,7 +488,6 @@ void draw_char(int x, int y, char c, Colour fg, const Colour *bg)
     case 'F':
       bits = 0xf8e88000;
       break;
-//
 //  XX  6
 // X    8
 // X XX b
@@ -220,7 +496,6 @@ void draw_char(int x, int y, char c, Colour fg, const Colour *bg)
     case 'G':
       bits = 0x68b96000;
       break;
-//
 // X  X 9
 // X  X 9
 // XXXX f
@@ -229,7 +504,6 @@ void draw_char(int x, int y, char c, Colour fg, const Colour *bg)
     case 'H':
       bits = 0x99f99000;
       break;
-//
 // XXXX f
 //  XX  6
 //  XX  6
@@ -238,7 +512,6 @@ void draw_char(int x, int y, char c, Colour fg, const Colour *bg)
     case 'I':
       bits = 0xf666f000;
       break;
-//
 //    X 1
 //    X 1
 //    X 1
@@ -247,7 +520,6 @@ void draw_char(int x, int y, char c, Colour fg, const Colour *bg)
     case 'J':
       bits = 0x11196000;
       break;
-//
 // X  X 9
 // X X  a
 // XX   c
@@ -256,7 +528,6 @@ void draw_char(int x, int y, char c, Colour fg, const Colour *bg)
     case 'K':
       bits = 0x9aca9000;
       break;
-//
 // X    8
 // X    8
 // X    8
@@ -265,7 +536,6 @@ void draw_char(int x, int y, char c, Colour fg, const Colour *bg)
     case 'L':
       bits = 0x8888f000;
       break;
-//
 // X  X 9
 // XXXX f
 // XXXX f
@@ -274,7 +544,6 @@ void draw_char(int x, int y, char c, Colour fg, const Colour *bg)
     case 'M':
       bits = 0x9ff99000;
       break;
-//
 // X  X 9
 // XX X d
 // XXXX f
@@ -283,7 +552,6 @@ void draw_char(int x, int y, char c, Colour fg, const Colour *bg)
     case 'N':
       bits = 0x9dfb9000;
       break;
-//
 //  XX  6
 // X  X 9
 // X  X 9
@@ -292,7 +560,6 @@ void draw_char(int x, int y, char c, Colour fg, const Colour *bg)
     case 'O':
       bits = 0x69996000;
       break;
-// 
 // XXX  e
 // X  X 9
 // XXX  e
@@ -301,7 +568,6 @@ void draw_char(int x, int y, char c, Colour fg, const Colour *bg)
     case 'P':
       bits = 0xe9e88000;
       break;
-//
 //  XX  6
 // X  X 9
 // X  X 9
@@ -310,7 +576,6 @@ void draw_char(int x, int y, char c, Colour fg, const Colour *bg)
     case 'Q':
       bits = 0x699b7000;
       break;
-//
 // XXX  e
 // X  X 9
 // XXX  e
@@ -319,7 +584,6 @@ void draw_char(int x, int y, char c, Colour fg, const Colour *bg)
     case 'R':
       bits = 0xe9e99000;
       break;
-//
 //  XXX 7
 // X    8
 //  XX  6
@@ -328,7 +592,6 @@ void draw_char(int x, int y, char c, Colour fg, const Colour *bg)
     case 'S':
       bits = 0x7861e000;
       break;
-//
 // XXXX f
 //  XX  6
 //  XX  6
@@ -337,7 +600,6 @@ void draw_char(int x, int y, char c, Colour fg, const Colour *bg)
     case 'T':
       bits = 0xf6666000;
       break;
-//
 // X  X 9
 // X  X 9
 // X  X 9
@@ -346,7 +608,6 @@ void draw_char(int x, int y, char c, Colour fg, const Colour *bg)
     case 'U':
       bits = 0x9999f000;
       break;
-//
 // X  X 9
 // X  X 9
 // X  X 9
@@ -355,7 +616,6 @@ void draw_char(int x, int y, char c, Colour fg, const Colour *bg)
     case 'V':
       bits = 0x99996000;
       break;
-//
 // X  X 9
 // X  X 9
 // XXXX f
@@ -364,7 +624,6 @@ void draw_char(int x, int y, char c, Colour fg, const Colour *bg)
     case 'W':
       bits = 0x99ff9000;
       break;
-//
 // X  X 9
 // X  X 9
 //  XX  6
@@ -373,7 +632,6 @@ void draw_char(int x, int y, char c, Colour fg, const Colour *bg)
     case 'X':
       bits = 0x99699000;
       break;
-//
 // X  X 9
 // X  X 9
 // X  X 9
@@ -382,7 +640,6 @@ void draw_char(int x, int y, char c, Colour fg, const Colour *bg)
     case 'Y':
       bits = 0x99966000;
       break;
-//
 // XXXX f
 //   XX 3
 //  XX  6
