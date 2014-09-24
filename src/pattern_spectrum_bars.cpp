827188d61bc91d38d8b7d7ca812b7231a78f57a2
David Riley <driley@moonfall.com>
2014-09-23 21:53:19 -0700

    lush: improve pattern_spectrum_bars peak hold

--- src/pattern_spectrum_bars.cpp
+++ src/pattern_spectrum_bars.cpp
@@ -12,35 +12,48 @@ void Pattern_spectrum_bars::activate(void *arg)
 // TODO: Change to constant.
 Bin_type g_peaks[COLUMN_COUNT];
 Colour g_peak_leds[COLUMN_COUNT];
+const int PEAK_FADE_TIME = 3;
 #endif
 
 bool Pattern_spectrum_bars::display()
 {
+    Colour c = make_current_hue();
     for (int x = 0; x < COLUMN_COUNT; ++x) {
 	Bin_type bin = g_bins[x];
 #ifdef PEAK_HOLD
-	if (bin > g_peaks[x]) {
-	    g_peaks[x] = bin;
-	    g_peak_leds[x] = make_current_hue();
+	if (bin * PEAK_FADE_TIME > g_peaks[x]) {
+	    g_peaks[x] = bin * PEAK_FADE_TIME;
+	    g_peak_leds[x] = c;
+	    g_peak_leds[x] = make_invert_hue();
 	}
 #endif
 	// How many rows are lit up because of the current sample.
 	int rows = bin;
 #ifdef PEAK_HOLD
 	// How many rows are lit up because of peak hold.
-	int peak_rows = g_peaks[x];
+	int peak_rows = g_peaks[x] / PEAK_FADE_TIME;
 #endif
+	Serial.print(rows);
+	Serial.print(" ");
+	Serial.print(peak_rows);
+	Serial.print(" ");
 	for (int y = 0; y < ROW_COUNT; ++y) {
-	    if (y < rows) {
-		draw_pixel(x, flip_y(y), make_current_hue());
 #ifdef PEAK_HOLD
-	    } else if (y < peak_rows) {
+	    // alternative: y < peak_rows
+	    if (y == peak_rows - 1) {
 		draw_pixel(x, flip_y(y), g_peak_leds[x]);
+		Serial.print("*");
+	    } else
 #endif
+	    if (y < rows) {
+		draw_pixel(x, flip_y(y), c);
+		Serial.print("r");
 	    } else {
 		draw_pixel(x, flip_y(y), COLOUR_BLACK);
+		Serial.print(" ");
 	    }
 	}
+	Serial.println();
 #ifdef PEAK_HOLD
 	// Change to time based.
 	if (g_peaks[x] > 0) {
